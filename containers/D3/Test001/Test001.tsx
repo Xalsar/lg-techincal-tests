66d2bb3bda0c61e67e3eec254438a080dbb6091b
XaviCarreraGimbert <xalsar@protonmail.com>
2023-05-28 18:37:19 +0200

    More upgrades to test chart

--- containers/D3/Test001/Test001.tsx
+++ containers/D3/Test001/Test001.tsx
@@ -11,27 +11,44 @@ const DUMMY_DATA = [
   { id: 2, value: 11, region: "India" },
   { id: 3, value: 12, region: "Taiwan" },
   { id: 4, value: 6, region: "Lichtenstein" },
+  { id: 5, value: 4, region: "Spain" },
+  { id: 6, value: 9, region: "Portugal" },
+  { id: 7, value: 3, region: "Japan" },
+  { id: 8, value: 10, region: "Korea" },
+  { id: 9, value: 3, region: "China" },
 ];
+
+const width = 800;
+const height = 400;
+
+const margin = { top: 20, right: 30, bottom: 55, left: 70 };
 
 const Test001 = () => {
   useEffect(() => {
     const xScale = d3
       .scaleBand()
       .domain(DUMMY_DATA.map((data) => data.region))
-      .rangeRound([0, 250])
+      .range([0, width])
       .padding(0.1);
-    const yScale = d3.scaleLinear().domain([0, 15]).range([200, 0]);
+    const yScale = d3
+      .scaleLinear()
+      .domain([0, Number(d3.max(DUMMY_DATA, (d) => d.value))])
+      .range([height, 0]);
 
-    d3.select("#chart-test-001")
+    const container = d3.select("#chart-test-001");
+
+    container
+      .attr("width", width)
+      .attr("height", height)
       .classed(classes.container, true)
+      // SELECT THE BARS
       .selectAll(classes.bar)
       .data(DUMMY_DATA)
-      .enter()
-      .append("rect")
+      .join("rect")
       .classed(classes.bar, true)
       .attr("width", xScale.bandwidth())
-      .attr("height", (data) => 200 - yScale(data.value))
-      .attr("x", (data) => xScale(data.region))
+      .attr("height", (data) => height - yScale(data.value))
+      .attr("x", (data) => String(xScale(data.region)))
       .attr("y", (data) => yScale(data.value));
   }, []);
 
