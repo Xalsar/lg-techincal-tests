a2a343c0da19660aea302791b222318e10935bd8
XaviCarreraGimbert <xalsar@protonmail.com>
2023-05-29 19:34:24 +0200

    More upgrades to map

--- containers/D3/Map/Map.tsx
+++ containers/D3/Map/Map.tsx
@@ -8,24 +8,33 @@ const svgId = "map-d3";
 
 const Map = () => {
   useEffect(() => {
-    const width = document.querySelector("body")!.clientWidth;
-    const height = 500;
+    (async () => {
+      const width = document.querySelector("body")!.clientWidth;
+      const height = 500;
 
-    const svg = d3.select("#" + svgId).attr("viewBox", [0, 0, width, height]);
+      // CONTAINER
+      const svg = d3.select("#" + svgId).attr("viewBox", [0, 0, width, height]);
 
-    const projection = d3.geoEquirectangular().center([0, 0]);
-    const pathGenerator = d3.geoPath().projection(projection);
+      // PROJECTION AND PATH
+      const projection = d3.geoEquirectangular().center([0, 0]);
+      const pathGenerator = d3.geoPath().projection(projection);
 
-    const g = svg.append("g");
+      const g = svg.append("g");
 
-    d3.json(
-      "https://raw.githubusercontent.com/iamspruce/intro-d3/main/data/countries-110m.geojson"
-    ).then((data: any) => {
-      g.selectAll("path")
-        .data(data.features)
-        .join("path")
-        .attr("d", (val: any) => pathGenerator(val));
-    });
+      try {
+        const data: any = await d3.json(
+          "https://raw.githubusercontent.com/iamspruce/intro-d3/main/data/countries-110m.geojson"
+        );
+
+        // DRAW PATHS
+        g.selectAll("path")
+          .data(data.features)
+          .join("path")
+          .attr("d", (val: any) => pathGenerator(val));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
   }, []);
 
   return <svg id={svgId} />;
